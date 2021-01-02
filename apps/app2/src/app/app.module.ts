26ee421a3915e1f5b07ea23ff89c1c1dbdec3689
Nilav Patel <nilavpatel1992@gmail.com>
2021-01-02 14:31:28 +0530

    update code

--- apps/app2/src/app/app.module.ts
+++ apps/app2/src/app/app.module.ts
@@ -2,17 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { HeaderModule } from 'libs/header/src/index'
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    HeaderModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
